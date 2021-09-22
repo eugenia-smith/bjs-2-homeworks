df3e140ae00861d786f60a1baba53c32a41273c4
eugenia-smith <ev-genius4@mail.ru>
2021-09-22 15:21:21 +0300

    Task 2 is completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -21,9 +21,30 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+ 
+  let percentage = parseInt(percent, 10);
+  let payment = parseInt(contribution, 10);
+  let creditSum = parseInt(amount, 10);
+  let currentDate = new Date();
+  let dateToPay = Date.parse(date);
+  
+  if (isNaN(percentage) === true) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (isNaN(payment) === true) {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  } else if (isNaN(creditSum) === true) {
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  } else if (dateToPay === 'undefined' || dateToPay <= Date.parse(currentDate)) {
+    return `Параметр "Дата" содержит неправильное значение ${date}`;
+  }
 
-  // код для задачи №2 писать здесь
+  let creditPeriod = Math.floor((dateToPay - Date.parse(currentDate)) / 86400000 / 30);
+  let creditBody = creditSum - payment;
+  let partPercentage = percentage / 1200;
+  let monthlyPayment = creditBody * (partPercentage + partPercentage / (((1 + partPercentage)**creditPeriod) - 1));
+  let totalAmount = parseFloat((monthlyPayment * creditPeriod).toFixed(2));
+
+console.log({percentage, partPercentage, payment, creditSum, creditPeriod, monthlyPayment, totalAmount});
 
   return totalAmount;
 }
