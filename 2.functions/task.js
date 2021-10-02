e0dde6b493d69d685945dadd86f5e30066fc9841
eugenia-smith <60076849+eugenia-smith@users.noreply.github.com>
2021-10-02 15:35:49 +0300

    Tasks 1 and 2 completed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,26 +1,47 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+
+    if (arr[i] > max) {
+      max = arr[i]
+    }
+
+    sum += arr[i];
+  }
+
+  avg = parseFloat((sum / arr.length).toFixed(2));
 
   return { min:min, max:max, avg:avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
+  for (let k = 0; k < arrOfArr.length; k++) {
+    worker(arrOfArr[k]);
+    if (worker(arrOfArr[k]) > max) {
+      max = worker(arrOfArr[k]);
+    }
+  }
   
   return max
 }
