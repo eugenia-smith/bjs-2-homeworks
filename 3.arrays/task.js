843e122c97fe2c5a1c18a98806cf48e2a3f6969c
eugenia-smith <60076849+eugenia-smith@users.noreply.github.com>
2021-10-10 22:51:59 +0300

    Task completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,9 @@
+"use strict"
+
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
+  
+  result = arr1.every((x, i) => {return x === arr2[i];}) && (arr1.length === arr2.length);
 
   return result; // boolean
 }
@@ -9,7 +11,7 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter((someLet) => {return someLet > 0}).filter((someLet) => {return someLet %3 === 0}).map((someLet) => {return someLet * 10});
 
   return resultArr; // array
 }
