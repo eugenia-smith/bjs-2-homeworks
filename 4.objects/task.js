003a694aca284b222fdc21985bfd8f3e0eae1401
eugenia-smith <60076849+eugenia-smith@users.noreply.github.com>
2021-10-11 23:14:37 +0300

    Task completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,41 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...rest) {
+  if (this.marks === undefined) {
+    this.marks = rest;
+  } else {
+    for (let i = 0; i < rest.length; i++) {
+      this.marks.push(rest[i]);
+    }
+  }
+
+  Student.prototype.getAverage = function () {
+    let sum = 0;
+    for (let k = 0; k < this.marks.length; k++)
+      sum += this.marks[k];
+
+    return sum / this.marks.length
+  }
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
